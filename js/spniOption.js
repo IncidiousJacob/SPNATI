fbb731790a14acb99d6de41f3a472fcb11e1a3fd
ReformCopyright <magnus@kibibyte.se>
2024-07-08 17:52:03 +0200

    Add a tooltip and make it appear automatically on the title screen until the player opens the background dialog once.
    Maybe a bit annoying but I want to limit the amount of code somewhat.
    
    I added a generic pair of method to record having shown hints like this in an array instead of individual localStorage slots.

--- js/spniOption.js
+++ js/spniOption.js
@@ -404,6 +404,9 @@ function loadBackgrounds() {
             defaultBackground = backgrounds[defaultBackgroundID];
         }
         $('.background-select-button').show();
+        if (!save.hasSeenHint('bgbutton')) {
+            $('#title-screen .background-select-button').tooltip('show');
+        }
     }).catch(function (err) {
         console.error("Could not load backgrounds:");
         captureError(err);
@@ -641,6 +644,8 @@ function showGameSettingsModal () {
     });
 
     $('#game-settings-modal').modal('show');
+    save.recordSeenHint('bgbutton');
+    $('#title-screen .background-select-button').tooltip('hide');
 }
 
 $('#game-settings-modal').on('shown.bs.modal', function() {
