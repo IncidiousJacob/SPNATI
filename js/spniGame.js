062423a3b053da3b1189d91ef5405ef6886968c2
ReformCopyright <magnus@kibibyte.se>
2024-10-11 23:20:01 +0200

    Since we now hide the player label (if auto fade is on), if we make invisible label not react to pointer events, we can let the auto advance buttons be shown one phase earlier.

--- js/spniGame.js
+++ js/spniGame.js
@@ -801,8 +801,8 @@ function allowProgression (nextPhase) {
     actualMainButtonState = false;
     timeoutID = undefined;
     allowAutoAdvance = nextPhase != eGamePhase.GAME_OVER && !inRollback()
-        && ((humanPlayer.out && ((humanPlayer.timer > 1 && humanPlayer.ticksInStage > 0) || gamePhase == eGamePhase.STRIP)
-             || humanPlayer.finished || (!humanPlayer.out && gameOver && players[recentLoser].ticksInStage > 0)));
+        && ((humanPlayer.out && (humanPlayer.timer > 1 || gamePhase == eGamePhase.STRIP)
+             || humanPlayer.finished || (!humanPlayer.out && gameOver)));
     $autoAdvanceButtons.toggle(allowAutoAdvance);
 
     if (autoAdvancePaused) {
