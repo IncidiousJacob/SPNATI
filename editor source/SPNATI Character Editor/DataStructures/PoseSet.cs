4dc92da57d6879257225f632023dc8c7830ab3fc
TreeWizard96 <treewizardthegreatest@gmail.com>
2025-05-17 21:07:21 +0100

    Initial implementation

--- editor source/SPNATI Character Editor/DataStructures/PoseSet.cs
+++ editor source/SPNATI Character Editor/DataStructures/PoseSet.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Xml.Serialization;
+using System.Linq;
+
 
 namespace SPNATI_Character_Editor
 {
@@ -41,6 +43,88 @@ namespace SPNATI_Character_Editor
 		{
 			return Id;
 		}
+
+		public bool HasCoverageForStages(List<int> caseStages)
+		{
+			var coveredStages = Entries.SelectMany(entry => ParseStageRange(entry.Stage)).ToHashSet();
+			return caseStages.All(stage => coveredStages.Contains(stage));
+		}
+
+
+		public PoseSetEntry GetPoseForStages(List<int> caseStages)
+		{
+			HashSet<int> coveredStages = new HashSet<int>();
+			foreach (var entry in Entries)
+			{
+				List<int> entryStages = ParseStages(entry.Stage);
+				foreach (int s in entryStages)
+					coveredStages.Add(s);
+			}
+
+			if (!caseStages.All(stage => coveredStages.Contains(stage)))
+				return null;
+
+			foreach (var entry in Entries)
+			{
+				List<int> entryStages = ParseStages(entry.Stage);
+				if (caseStages.Any(stage => entryStages.Contains(stage)))
+					return entry;
+			}
+
+			return null;
+		}
+
+		private IEnumerable<int> ParseStageRange(string stageRange)
+		{
+			if (string.IsNullOrEmpty(stageRange)) yield break;
+
+			foreach (var part in stageRange.Split(','))
+			{
+				if (part.Contains('-'))
+				{
+					var bounds = part.Split('-');
+					if (int.TryParse(bounds[0], out int start) && int.TryParse(bounds[1], out int end))
+					{
+						for (int i = start; i <= end; i++)
+							yield return i;
+					}
+				}
+				else if (int.TryParse(part, out int singleStage))
+				{
+					yield return singleStage;
+				}
+			}
+		}
+
+		private List<int> ParseStages(string stageRange)
+		{
+			List<int> stages = new List<int>();
+
+			if (string.IsNullOrEmpty(stageRange))
+				return stages;
+
+			foreach (var part in stageRange.Split(','))
+			{
+				if (part.Contains("-"))
+				{
+					var bounds = part.Split('-');
+					if (int.TryParse(bounds[0], out int start) && int.TryParse(bounds[1], out int end))
+					{
+						for (int i = start; i <= end; i++)
+						{
+							stages.Add(i);
+						}
+					}
+				}
+				else if (int.TryParse(part, out int singleStage))
+				{
+					stages.Add(singleStage);
+				}
+			}
+
+			return stages;
+		}
+
 
 	}
 
