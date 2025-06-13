36a0d17d96413558757661e425ee252d2e04f617
ReformCopyright <magnus@kibibyte.se>
2025-06-13 21:43:14 +0200

    Parse scores as numbers when reading listing.xml.
    Thus `sortOpponentsByField()` shouldn't need to be be changed.

--- js/spniSelect.js
+++ js/spniSelect.js
@@ -421,11 +421,11 @@ function loadListingFile () {
                 releaseNumber = Number(releaseNumber);
             }
             var highlightStatus = $(this).attr('highlight');
-            var rosterScore = $(this).attr('score');
+            var rosterScore = Number.parseFloat($(this).attr('score'), 10);
             var addedDate = $(this).attr('addedDate');
 
             // Keep the unsorted characters in order
-            if (rosterScore === undefined) {
+            if (Number.isNaN(rosterScore)) {
                 rosterScore = unsortedScore;
                 unsortedScore -= 1;
             }
